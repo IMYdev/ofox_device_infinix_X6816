b014b3cd503961ff7879a82f34bed06098064535
IMYdev <omranmansour096@gmail.com>
2023-12-24 19:00:31 +0200

    Fix sepolicy implementation

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,7 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # Sepolicy
-BOARD_SEPOLICY_DIRS := device/infinix/lion/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Hardware
 BOARD_USES_MTK_HARDWARE := true
